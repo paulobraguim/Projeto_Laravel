07b188bc278fa1ad3f8151a9a132f7bb9c25854d
Paulo <paulo.braguim@outlook.com>
2021-09-18 11:02:20 -0300

    Projeto Laravel

--- routes/web.php
+++ routes/web.php
@@ -16,16 +16,33 @@ use App\Http\Controllers\ContatoController;
 |
 */
 
-Route::get('/', [PrincipalController::class, 'principal']);
-Route::get('/sobre-nos', [SobreNosController::class, 'sobreNos']);
-Route::get('/contato', [ContatoController::class, 'contato']);
-Route::get('/login', function(){return 'Login';});
+Route::get('/', [PrincipalController::class, 'principal'])->name('site.index');
+Route::get('/sobre-nos', [SobreNosController::class, 'sobreNos'])->name('site.sobrenos');
+Route::get('/contato', [ContatoController::class, 'contato'])->name('site.contato');
+Route::get('/login', function(){return 'Login';})->name('site.login');
 
 /**
  * Agrupamento de Rotas
  */
 Route::prefix('/adm')->group(function(){
-    Route::get('/clientes', function(){return 'Clientes';});
-    Route::get('/fornecedores', function(){return 'Fornecedores';});
-    Route::get('/produtos', function(){return 'Produtos';});
+    Route::get('/clientes', function(){return 'Clientes';})->name('adm.clientes');
+    Route::get('/fornecedores', function(){return 'Fornecedores';})->name('adm.fornecedores');
+    Route::get('/produtos', function(){return 'Produtos';})->name('adm.produtos');
+});
+
+/**
+ * Rotas de Teste
+ */
+Route::get('/rota1', function(){
+    echo "Rota1";
+})->name('site.rota1');
+
+Route::get('/rota2', function(){
+    return redirect()->route('site.rota1');
+})->name('site.rota2');
+    
+//Route::redirect('/rota2', '/rota1');
+
+Route::fallback(function(){
+    echo 'Rota não encontrada. Clique <a href="'.route('site.index').'">aqui</a> para voltar.';
 });
