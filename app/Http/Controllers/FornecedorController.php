9afe1809845ec185d5a07f479fd0f53d565ac300
Paulo <paulo.braguim@outlook.com>
2021-09-21 09:47:27 -0300

    Projeto Laravel

--- app/Http/Controllers/FornecedorController.php
+++ app/Http/Controllers/FornecedorController.php
@@ -7,6 +7,9 @@ use Illuminate\Http\Request;
 class FornecedorController extends Controller
 {
     public function index(){
-        return view('app.fornecedor.index');
+        $fornecedores = [
+            0 => ['nome' => 'Fornecedor 1', 'status' => 'N']
+        ];
+        return view('app.fornecedor.index', compact('fornecedores'));
     }
 }
--- resources/views/app/fornecedor/index.blade.php
+++ resources/views/app/fornecedor/index.blade.php
@@ -3,5 +3,30 @@
 {{-- Comentário syntax Blade --}}
 
 @php
-    // Bloco php na syntax Blade
+    /*
+        @if(count($fornecedores) > 0 && count($fornecedores) < 10)
+            <h3>Existem alguns fornecedores cadastrados</h3>
+        @elseif(count($fornecedores) > 10)
+            <h3>Existem vários fornecedores cadastrados</h3>
+        @else 
+            <h3>Ainda não existem fornecedores cadastrados</h3>
+        @endif
+
+        ----------------------------------------------------------
+        Operador de negação !
+        @unless($fornecedores[0]['status'] == 'S')
+            Fornecedor Inativo
+        @endunless
+    
+    */
 @endphp
+
+Fornecedor: {{ $fornecedores[0]['nome'] }}
+<br>
+Status: {{ $fornecedores[0]['status'] }}
+<br>
+
+@unless($fornecedores[0]['status'] == 'S')
+    Fornecedor Inativo
+@endunless    
+           
