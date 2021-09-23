115328a9d79d5554b439368cfbfacd2c90c48bea
Paulo Henrique <paulo.braguim@outlook.com>
2021-09-22 22:55:48 -0300

    Otimizando o CSS

--- resources/views/site/principal.blade.php
+++ resources/views/site/principal.blade.php
@@ -4,99 +4,10 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Contato</title>    
+    <title>Home</title>    
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
-
-    <style>
-        @import url('https://fonts.googleapis.com/css2?family=Roboto:ital@1&display=swap');
-
-        .container{
-            padding: 70px 0;
-        }
-
-        p, li{
-            color: #444;
-            font-size: 14px;;
-        }
-
-        /* NAV */
-        header, .navbar{
-            background-color: #2908a1;
-            font-family: 'Roboto';
-        }
-
-        #nav-container{
-            padding-top: 0;
-            padding-bottom: 0;
-        }
-
-        #logo{
-            width: 75px;
-        }
-
-        .navbar-brand{
-            padding: 0;
-            color: #fff;
-        }
-
-        .navbar-brand:hover{
-            color: #fff;
-        }
-
-        #navbar-links{
-            margin-right: 60px;    
-        }
-
-        #navbar-links a{
-            color: #fff;
-            text-transform: uppercase;    
-        }
-
-        #navbar-links a:hover{
-            color: #eed812;
-            text-transform: 3s;
-            font-weight: bold;
-        }
-
-        .navbar-expand-lg .navbar-nav .nav-link{
-            padding: 1rem .8rem;
-        }
-
-        .row-edit{
-            text-align: center;            
-            margin-top: 40px;
-        }
-
-        .row-edit-form{            
-            margin-top: 40px;
-            padding: 5px;
-        }
-
-        .row-edit p{
-            font-weight: 300;
-            font-size: 15px;
-        }
-
-        .img-edit{
-            width: 450px;
-            height: 300px;
-        }
-
-        footer{
-            background-color: #2908a1;
-            height: 250px;
-            width: auto;            
-            font-weight: 300;
-            text-align: center;
-        }
-
-        footer p{
-            color: #fff;
-            font-size: 17px;;
-        }
-
-    </style>
+    <link rel="stylesheet" href="{{ asset('css/style_base.css') }}">
 </head>
 <body>
     <header>
