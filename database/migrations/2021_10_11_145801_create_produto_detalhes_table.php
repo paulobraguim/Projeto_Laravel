50755273ea08a6f58cfa51f7376bc9812ba3c153
Paulo Henrique <paulo.braguim@outlook.com>
2021-10-11 12:13:25 -0300

    Criando relação da tabela produtos

---
+++ database/migrations/2021_10_11_145801_create_produto_detalhes_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProdutoDetalhesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('produto_detalhes', function (Blueprint $table) {
+            //Criação das Colunas
+            $table->id();
+            $table->float('comprimento', 8, 2);
+            $table->float('largura', 8, 2);
+            $table->float('altura', 8, 2);
+            $table->unsignedBigInteger('produto_id');
+            $table->timestamps();
+
+            //Criação da constraint
+            $table->foreign('produto_id')->references('id')->on('produtos');
+            
+            //Garante que o relacionamento seja de 1 para 1
+            $table->unique('produto_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('produto_detalhes');
+    }
+}
